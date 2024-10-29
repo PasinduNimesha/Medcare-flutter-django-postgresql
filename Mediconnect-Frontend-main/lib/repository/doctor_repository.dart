37a00829cbced99bff03f55a1e0380481379dd2a
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-10-29 18:46:27 +0530

    Update doctor_repository.dart

--- Mediconnect-Frontend-main/lib/repository/doctor_repository.dart
+++ Mediconnect-Frontend-main/lib/repository/doctor_repository.dart
@@ -1,16 +1,21 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class DoctorRepository {
+  http.Client client;
+
+  // Inject the client dependency (defaulting to http.Client)
+  DoctorRepository({http.Client? client}) : client = client ?? http.Client();
+
   Future<dynamic> createDoctor({required String doctor}) async {
     try {
-      var response = await http.post(
-          Uri.parse("http://10.0.2.2:8000/api/doctors/create/"),
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8'
-          },
-          body: doctor);
+      var response = await client.post(
+        Uri.parse("http://10.0.2.2:8000/api/doctors/create/"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: doctor,
+      );
       final data = jsonDecode(response.body);
       if (data == null) {
         return {"status": "error", "message": "Something went wrong"};
@@ -19,6 +24,7 @@ class DoctorRepository {
       }
     } catch (error) {
       print("Error: " + error.toString());
+      throw Exception("Failed to create doctor");
     }
   }
 }
